a8898603b17ca1190ee781378e524f1c3b60e235
Frank <mailcvv3@gmail.com>
2025-08-18 09:26:59 +0700

    INIT3

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,10 +11,12 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
+      // should i use config service?
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
